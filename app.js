ed7ec22bc71f77b49fc4cf4cbd98d99903bacbc8
Bernardo Paes <bersantos22@gmail.com>
2022-02-21 18:32:45 -0300

    done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { all } = require("express/lib/application");
 
-const PORT = 4000;
+const PORT = 5000;
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
@@ -8,5 +9,18 @@ const allPokemon = require("./data");
 const app = express();
 
 // -- Define your route listeners here! --
+
+
+app.get("/pokemon", (req, res) => {
+    res.json(allPokemon);
+  });
+
+
+   app.get("/pokemon/:id", (req, res) => {
+    const { id } = req.params;
+    const pk = allPokemon.find((pk) => pk.id == id);
+    res.json(pk);
+  });
+
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
